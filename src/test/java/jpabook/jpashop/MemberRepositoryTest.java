c75343fc549085178b230d8b40e71a9afe194653
BAEKJungHo <designjava@naver.com>
2021-02-28 12:56:17 +0900

    yml 수정
    H2 1.4.200 버전부터 MVCC 옵션이 제거되었다.

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,11 +1,14 @@
 package jpabook.jpashop;
 
 import com.fasterxml.jackson.databind.deser.std.StdKeyDeserializer;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.*;
 
@@ -15,6 +18,31 @@ public class MemberRepositoryTest {
 
     @Autowired
     MemberRepository memberRepository;
+
+    /**
+     * @Transactional 은 자바와 Spring 둘다 있는데 스프링을 사용하고 있으니 스프링으로 쓰는것을 권장
+     * 쓸 수 있는 옵션이 많음
+     * EntityManager 를 사용할 때 트랜잭션이 걸려있지 않으면
+     * No EntityManager with actual transaction available ... 이라는 에러가 발생한다.
+     * @Transactional 이 테스트 케이스에 있으면 테스트가 끝나고 Rollback 을 해버린다.
+     * @Rollback(false) 을 하면 롤백을 하지 않는다.
+     * @throws Exception
+     */
+    @Test
+    @Transactional
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long savedId = memberRepository.save(member);
+        Member findMember  = memberRepository.find(savedId);
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
 
 
 }
